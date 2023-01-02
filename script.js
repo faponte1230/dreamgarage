b1074541283120f849dfe12335ceb5c97b5002a6
Fernando Aponte <faponte1230@yahoo.com>
2023-01-02 07:27:48 -0500

    added code

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', fetchCars)
+
+function fetchCars(){
     fetch('http://localhost:3000/cars')
-    .then(res => res.json())
-    .then(data => console.log(data))
-})
+    .then(res => res.json())   
+    .then(carData => integrate(carData))
+}
+
+function integrate(carData){
+    carData.forEach(car => {
+        renderCarCard(car)
+    })
+}
+
+let carContainer = document.querySelector('.car-container')
+
+function renderCarCard(car){
+    let card = document.createElement('div')
+    card.className = 'card'
+    let img = document.createElement('img')
+    img.src = car.img
+    img.className = 'car-img'
+}
+
