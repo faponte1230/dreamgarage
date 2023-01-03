e76536941b8e5d51df1891ea187720d924eed489
Fernando Aponte <faponte1230@yahoo.com>
2023-01-02 23:58:34 -0500

    update project files

--- script.js
+++ script.js
@@ -2,27 +2,69 @@ document.addEventListener('DOMContentLoaded', fetchCars)
 
 function fetchCars(){
     fetch('http://localhost:3000/cars')
-    .then(res => res.json())   
-    .then(carData => integrate(carData))
+    .then(resp => resp.json())   
+    .then(carsData => renderCarCard(carsData))    
 }
 
-function integrate(carData){
-    carData.forEach(car => {
-        renderCarCard(car)
+    function renderCarCard(carsData){
+    carsData.forEach(car => {
+        let carContainer = document.querySelector('.car-container')    
+        let card = document.createElement('div')
+        card.className = 'card'
+        let vehicle = document.createElement('div')
+        vehicle.textContent = car.name 
+        const img = document.createElement('img')
+        img.src = car.imgUrl
+        img.className = 'car-img'
+        const btn = document.createElement('button')
+        btn.textContent = 'TOW AWAY'
+        btn.className = 'delete'
+        btn.addEventListener('click' , () =>card.remove())
+        card.append(vehicle, img, btn)
+        carContainer.appendChild(card)
+        sendUpdateToServer(car)
+    })
+    }
+
+    
+    
+// PATCH method used to send the updated board quantity to the server
+function sendUpdateToServer(car){
+    fetch(`http://localhost:3000/cars/${car.id}`,{
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application.json'
+        },
+        body:JSON.stringify(car)
     })
 }
 
-let carContainer = document.querySelector('.car-container')
+//used submit event listener 
+let form = document.querySelector('form')
+form.addEventListener('submit', createNewCar)
 
-function renderCarCard(car){
-    let card = document.createElement('div')
-    card.className = 'card'
-    let vehicle = document.createElement('div')
-    vehicle.textContent = 'cars.name' 
-    let img = document.createElement('img')
-    img.src = car.img
-    img.className = 'car-img'
-    card.append(img, vehicle)
-    carContainer.append(card)
+function createNewCar(e){
+e.preventDefault()
+let newCar = {
+    'name': e.target.name.value,
+    'image': e.target.image.value
+}
+submitNewCarToDb(newCar)
+
+form.reset()
 }
 
+// use post method to send new boards submitted through the form to the DB
+function submitNewCarToDb(newCar){
+fetch('http://localhost:3000/cars',{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+    },
+    body: JSON.stringify(newCar)
+})
+.then(resp => resp.json())
+.then(car => renderCarCard(car))
+}
