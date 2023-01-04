b9c845ac7f79520ef807ff92172277d20cd81fee
Fernando Aponte <faponte1230@yahoo.com>
2023-01-03 20:05:08 -0500

    updated/tweaked files

--- script.js
+++ script.js
@@ -9,24 +9,40 @@ function fetchCars(){
     function renderCarCard(carsData){
     carsData.forEach(car => {
         let carContainer = document.querySelector('.car-container')    
+        
         let card = document.createElement('div')
         card.className = 'card'
+        
         let vehicle = document.createElement('div')
         vehicle.textContent = car.name 
+        
         const img = document.createElement('img')
         img.src = car.imgUrl
         img.className = 'car-img'
-        const btn = document.createElement('button')
+        let btn = document.createElement('button')
         btn.textContent = 'TOW AWAY'
         btn.className = 'delete'
         btn.addEventListener('click' , () =>card.remove())
+
         card.append(vehicle, img, btn)
         carContainer.appendChild(card)
         sendUpdateToServer(car)
     })
     }
 
-    
+    // use post method to send new cars submitted to the server
+function sendNewCarToServer(newCar){
+    fetch('http://localhost:3000/cars',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(newCar)
+    })
+    .then(resp => resp.json())
+    .then(car => renderCarCard(newCar))
+    }
     
 // PATCH method used to send new car to the server
 function sendUpdateToServer(car){
@@ -40,31 +56,24 @@ function sendUpdateToServer(car){
     })
 }
 
+
+
+
 // form listener
 let form = document.querySelector('form')
 form.addEventListener('submit', createNewCar)
 
 function createNewCar(e){
-e.preventDefault()
+/*let carName = document.getElementsByClassName('input-car-name')
+console.log(carName)*/
+
 let newCar = {
-    'name': e.target.name.value,
-    'imgUrl': e.target.imgUrl
+    'name': e.target.name.value ,
+    'imgUrl' : e.target.img
 }
-submitNewCarToDb(newCar)
+sendNewCarToServer(newCar)
 
 form.reset()
 }
 
-// use post method to send new cars submitted  to the DB
-function submitNewCarToDb(newCar){
-fetch('http://localhost:3000/cars',{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-    },
-    body: JSON.stringify(newCar)
-})
-.then(resp => resp.json())
-.then(car => renderCarCard($$$$))
-}
+
